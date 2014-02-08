b9e0663038e56723026f19dfa6b4b9003dd754e1
Jaan Pullerits <clysuva@gmail.com>
2014-02-08 18:25:33 +0200

    Add basic skeleton.

---
+++ src/Colors.js
@@ -0,0 +1,9 @@
+define([], function(){
+    return {
+        darkBlue: "0E2F57",
+        blue: "15467F",
+        lightBlue: "0075FF",
+        darkOrange: "B27300",
+        orange: "FFA400"
+    };
+});
